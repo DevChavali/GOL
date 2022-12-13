d6e6b3dc260f5cabc4786449c522433c61016dee
Aryadev Chavali <aryadev@aryadevchavali.com>
2022-12-13 18:08:16 +0000

    (game.py)+simple game of life implementation

---
+++ game.py
@@ -0,0 +1,52 @@
+"""
+Rules:
+Any living cell with 2 or 3 neighbours lives
+Any dead cell with 3 neigbours is born
+Otherwise stay the same
+"""
+
+def generate_grid(n:int = 512)->list:
+    return [[False for j in range(n)] for i in range(n)]
+
+def print_grid(grid:list):
+    for row in grid:
+        for cell in row:
+            print("1" if cell else "0", end = " ")
+        print()
+
+def surrounding(x:int, y:int, grid:list)->list:
+    """Return (at most) eight neigbours of the given coordinate in the grid"""
+    def lim_calc(a):
+        if a == 0:
+            return (0, 2)
+        elif a == len(grid) - 1:
+            return (len(grid) - 2, len(grid))
+        else:
+            return (a-1, a+2)
+    y_min, y_max = lim_calc(y)
+    x_min, x_max = lim_calc(x)
+
+    cells = [[grid[j][i] for j in range(y_min, y_max)
+              if j != y or i != x]
+             for i in range(x_min, x_max)]
+
+    return [item for sublist in cells for item in sublist]
+
+def flip_cell(grid: list, pos)->list:
+    x,y = pos
+    grid[y][x] = not grid[y][x]
+    return grid
+
+def tick_grid(grid: list)->list:
+    new_grid = generate_grid(len(grid))
+    for j in range(len(grid)):
+        for i in range(len(grid)):
+            cell = grid[j][i]
+            neighbours = surrounding(i, j, grid)
+            living = len(list(filter(lambda x: x, neighbours)))
+            if cell and (living != 2 and living != 3):
+                cell = False
+            elif (not cell) and (living == 3):
+                cell = True
+            new_grid[j][i] = cell
+    return new_grid
